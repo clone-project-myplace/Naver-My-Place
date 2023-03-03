a155c050c40e02821cbac8d56c47b1ddbed5a515
choi <wjfekr1@naver.com>
2023-03-03 16:02:50 +0900

    setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Router from "./pages/router";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Router />
+  </QueryClientProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
